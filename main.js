4d415e5cc9ae8e39bed39cf537158ace98a7a451
iratze <iratze.eizaguirre@gmail.com>
2023-12-07 19:36:30 +0000

    Un porrón de commits que deberían haberse hecho (estilado básico, creación de función constructora Player, añadido método insertPlayer e implementado movimiento ortopédico del player)

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log('Estoy bien linkeado')
+var board = document.querySelector('#main-board')
+
+function Player(x, y) {
+  this.x = x
+  this.y = y
+  this.direction = 0
+
+  this.sprite
+
+  this.insertPlayer = function() {
+    var newPlayer = document.createElement('div')
+    newPlayer.setAttribute('id', 'player')
+    newPlayer.style.top = this.y + 'px'
+    newPlayer.style.left = this.x + 'px'
+    board.appendChild(newPlayer)
+    this.sprite = newPlayer
+  }
+
+  this.move = function() {
+    this.x += 10 * this.direction
+    this.sprite.style.left = this.x + 'px'
+  }
+}
+
+var player = new Player(225, 750)
+player.insertPlayer()
+
+window.addEventListener( 'keydown', function(e) {
+  console.log(e.key)
+  switch(e.key) {
+    case 'a':
+      player.direction = -1
+      break
+    case 'd':
+      player.direction = +1
+      break
+  }
+  player.move()
+})
